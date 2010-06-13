e59fa5b1707c24194e793c3f40e0b79697862fd0
Ben McRedmond <ben@benmcredmond.com>
2010-06-13 02:02:04 +0100

    current_page was broken for index. Fixed, gem version bumped

--- lib/bolt/serve_page.rb
+++ lib/bolt/serve_page.rb
@@ -4,7 +4,7 @@ DRb.start_service()
 @serve_instance = DRbObject.new(nil, $drb_uri)
 
 def page(path, &block)
-  path = "" if path == "index"
   @current_page = path
+  path = "" if path == "index"  
   @serve_instance.pages[path] = block
 end
